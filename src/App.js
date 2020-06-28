2a8f3cfef66228e44ac51676f4b3b7c2e1643f10
devmahmud <expelmahmud@gmail.com>
2020-06-28 14:54:38 +0600

    Navbar and Drawer added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Home from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Home />
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import Drawer from "@material-ui/core/Drawer";
+import Box from "@material-ui/core/Box";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Avatar from "@material-ui/core/Avatar";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
+import ArrowBack from "@material-ui/icons/ArrowBack";
+import AssignmentInd from "@material-ui/icons/AssignmentInd";
+import Home from "@material-ui/icons/Home";
+import Apps from "@material-ui/icons/Apps";
+import ContactMail from "@material-ui/icons/ContactMail";
+import { makeStyles } from "@material-ui/core/styles";
+import avatar from "../avatar.png";
+
+const useStyles = makeStyles((theme) => ({
+  appbar: {
+    background: "#222",
+    margin: 0,
+  },
+  arrow: {
+    color: "tomato",
+  },
+  title: {
+    color: "tan",
+  },
+  menuSliderContainer: {
+    width: 250,
+    background: "#511",
+    height: "100%",
+  },
+  avatar: {
+    display: "block",
+    margin: "0.5rem auto",
+    width: theme.spacing(13),
+    height: theme.spacing(13),
+  },
+  listItem: {
+    color: "tan",
+  },
+}));
+
+const menuItems = [
+  { listIcon: <Home />, listText: "Home" },
+  { listIcon: <AssignmentInd />, listText: "Resume" },
+  { listIcon: <Apps />, listText: "Portfolio" },
+  { listIcon: <ContactMail />, listText: "Contact" },
+];
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const classes = useStyles();
+
+  const sideList = () => (
+    <Box className={classes.menuSliderContainer} component="div">
+      <Avatar className={classes.avatar} src={avatar} alt="Mahmudul Alam" />
+      <Divider />
+      <List>
+        {menuItems.map((item, i) => (
+          <ListItem
+            button
+            key={i}
+            className={classes.listItem}
+            onClick={() => setOpen(false)}
+          >
+            <ListItemIcon className={classes.listItem}>
+              {item.listIcon}
+            </ListItemIcon>
+            <ListItemText primary={item.listText} />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <React.Fragment>
+      <Box component="nav">
+        <AppBar position="static" className={classes.appbar}>
+          <Toolbar>
+            <IconButton onClick={() => setOpen(true)}>
+              <ArrowBack className={classes.arrow} />
+            </IconButton>
+            <Typography variant="h5" className={classes.title}>
+              Portfolio
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Drawer open={open} anchor="right" onClose={() => setOpen(false)}>
+        {sideList()}
+      </Drawer>
+    </React.Fragment>
+  );
+};
+
+export default Navbar;
